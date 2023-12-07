d3295f74e5d717c442a5b9c9c86f6009ac2a5bc8
jameshope87 <jameshope87@users.noreply.github.com>
2023-12-07 23:01:53 +0000

    Completed day 7

--- day07/day7.py
+++ day07/day7.py
@@ -4,18 +4,40 @@ from collections import Counter
 FILEPATH = 'day07/day7input'
 TESTPATH = 'day07/day7test'
 CARDLIST = '23456789TJQKA'
+TYPES = [
+    'High Card', 'Pair', 'Two Pair', 'Three of a Kind', 'Full House',
+    'Four of a Kind', 'Five of a Kind'
+]
 
 
 class Hand:
 
   def __init__(self, cards, bid):
     self.cards = cards
-    self.cardString = ''.join(cards).replace('A', 'E').replace(
-        'J', 'B').replace('Q', 'C').replace('K', 'D').replace('T', 'A')
+    self.cardString = [
+        ''.join(cards).replace('A', 'E').replace('J', 'B').replace(
+            'Q', 'C').replace('K', 'D').replace('T', 'A'),
+        ''.join(cards).replace('A', 'E').replace('J', '1').replace(
+            'Q', 'C').replace('K', 'D').replace('T', 'A')
+    ]
     self.type = 0
     self.rank = 0
     self.bid = bid
-    self.firstCard = CARDLIST.index(self.cards[0])
+
+  def wildCardRankup(self):
+    if (self.type == 6 and 'J' in self.cards) or (self.type == 5
+                                                  and 'J' in self.cards):
+      self.type = 7
+    elif (self.type == 4
+          and 'J' in self.cards) or (self.type == 3
+                                     and self.cards.count('J') == 2):
+      self.type = 6
+    elif self.type == 3 and self.cards.count('J') == 1:
+      self.type = 5
+    elif self.type == 2 and 'J' in self.cards:
+      self.type = 4
+    elif self.type == 1 and 'J' in self.cards:
+      self.type = 2
 
   def determineHandType(self):
     counter = Counter(self.cards)
@@ -24,7 +46,7 @@ class Hand:
       self.type = 7
     elif cardCounts[0][1] == 4:
       self.type = 6
-    elif cardCounts[0][1] == 4 and cardCounts[1][1]:
+    elif cardCounts[0][1] == 3 and cardCounts[1][1] == 2:
       self.type = 5
     elif cardCounts[0][1] == 3:
       self.type = 4
@@ -48,43 +70,46 @@ def parseData(data):
   return hands
 
 
-def listSorter(items):
+def listSorter(items, part):
   for i in range(1, len(items)):
     current = items[i]
     i2 = i
-    while i2 > 0 and items[i2 - 1].cardString > current.cardString:
+    while i2 > 0 and items[i2 - 1].cardString[part - 1] > current.cardString[
+        part - 1]:
       items[i2] = items[i2 - 1]
       i2 -= 1
     items[i2] = current
 
 
-def main(bin=True):
+def main(part=1):
   data = filehandling(FILEPATH)
   hands = parseData(data)
   for hand in hands:
     hand.determineHandType()
+    if part == 2:
+      hand.wildCardRankup()
   highCard = [hand for hand in hands if hand.type == 1]
-  listSorter(highCard)
+  listSorter(highCard, part)
   pairs = [hand for hand in hands if hand.type == 2]
-  listSorter(pairs)
+  listSorter(pairs, part)
   twoPairs = [hand for hand in hands if hand.type == 3]
-  listSorter(twoPairs)
+  listSorter(twoPairs, part)
   threeKind = [hand for hand in hands if hand.type == 4]
-  listSorter(threeKind)
+  listSorter(threeKind, part)
   fullHouse = [hand for hand in hands if hand.type == 5]
-  listSorter(fullHouse)
+  listSorter(fullHouse, part)
   fourKind = [hand for hand in hands if hand.type == 6]
-  listSorter(fourKind)
+  listSorter(fourKind, part)
   fiveKind = [hand for hand in hands if hand.type == 7]
-  listSorter(fiveKind)
+  listSorter(fiveKind, part)
   sortedHands = highCard + pairs + twoPairs + threeKind + fullHouse + fourKind + fiveKind
   p = 0
   for rank, hand in enumerate(sortedHands):
     wins = hand.bid * (rank + 1)
     p += wins
-    print(
-        f'Hand {"".join(hand.cards)} has rank {rank+1}, bid {hand.bid} and wins {wins}'
-    )
+#    print(
+#        f'Hand {"".join(hand.cards)} is a {TYPES[hand.type - 1]} has #rank {rank+1}, bid {hand.bid} and wins {wins}. The total is {p}.'
+#    )
   return p
 
 
--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from day07.day7 import main
-import time
+import day05.day5part2
 
 if __name__ == "__main__":
-  part1 = main()
-  print(f'The answer to Part 1 is {part1}')
+  pass
